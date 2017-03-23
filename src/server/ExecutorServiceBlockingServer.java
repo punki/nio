3cb4a2f8e584cf03e435a75db53db8e777b63371
Tomasz Pankowski <tomasz.pankowski@allegrogroup.com>
2017-03-23 18:33:16 +0100

    fixed thread pool - limited queue

--- src/server/ExecutorServiceBlockingServer.java
+++ src/server/ExecutorServiceBlockingServer.java
@@ -4,12 +4,16 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class ExecutorServiceBlockingServer {
     public static void main(String[] args) throws IOException {
         ServerSocket ss = new ServerSocket(8080);
-        ExecutorService pool = Executors.newFixedThreadPool(10);
+        ExecutorService pool = new ThreadPoolExecutor(10, 100,
+                0L, TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<>(1000)); // extra work will wait in the queue (but unbounded so it can crash)
         Handler<Socket, IOException> handler = new ExecutorServiceHandler<>(pool,new ExceptionHandler<>(
                 new PrintingHandler<>(new TransmogrifyHandler()),
                 throwable -> System.out.println("throwable = " + throwable)
